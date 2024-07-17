d03a472aa65afc26a8be37c923b2f8eb559f282d
Shoyaib <shoyaibalsakib@gmail.com>
2024-07-17 14:47:48 +0600

    Responsive Navigation Bar design using Tailwind CSS

---
+++ dist/assets/custom.js
@@ -0,0 +1,7 @@
+const mobil_icon = document.getElementById("mobile_icon");
+const maniMenu = document.getElementById("manu_main");
+
+mobil_icon.addEventListener("click", ()=>{
+    maniMenu.classList.toggle("hidden");
+
+})
